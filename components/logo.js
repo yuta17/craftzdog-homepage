b91baf34eb3d665b87ff0e315b3a2d401c33eee8
yuta17 <usgitan@gmail.com>
2021-11-05 12:37:19 +0900

    Fix author name

--- components/logo.js
+++ components/logo.js
@@ -1,6 +1,6 @@
 import Link from 'next/link'
+import Image from 'next/image'
 import { Text, useColorModeValue } from '@chakra-ui/react'
-import FootprintIcon from './icons/footprint'
 import styled from '@emotion/styled'
 
 const LogoBox = styled.span`
@@ -12,33 +12,35 @@ const LogoBox = styled.span`
   line-height: 20px;
   padding: 10px;
 
-  > svg {
+  img {
     transition: 200ms ease;
   }
 
-  &:hover > svg {
+  &:hover img {
     transform: rotate(20deg);
   }
 `
 
 const Logo = () => {
+  const footPrintImg = `/images/footprint${useColorModeValue('', '-dark')}.png`
+
   return (
-    (<Link href="/" scroll={false}>
-
-      <LogoBox>
-        <FootprintIcon />
-        <Text
-          color={useColorModeValue('gray.800', 'whiteAlpha.900')}
-          fontFamily='M PLUS Rounded 1c", sans-serif'
-          fontWeight="bold"
-          ml={3}
-        >
-          Takuya Matsuyama
-        </Text>
-      </LogoBox>
-
-    </Link>)
-  );
+    <Link href="/">
+      <a>
+        <LogoBox>
+          <Image src={footPrintImg} width={20} height={20} alt="logo" />
+          <Text
+            color={useColorModeValue('gray.800', 'whiteAlpha.900')}
+            fontFamily='M PLUS Rounded 1c", sans-serif'
+            fontWeight="bold"
+            ml={3}
+          >
+            Hassan
+          </Text>
+        </LogoBox>
+      </a>
+    </Link>
+  )
 }
 
 export default Logo
--- pages/index.js
+++ pages/index.js
@@ -43,7 +43,7 @@ const Home = () => (
       <Box display={{ md: 'flex' }}>
         <Box flexGrow={1}>
           <Heading as="h2" variant="page-title">
-            Takuya Matsuyama
+            Hassan
           </Heading>
           <p>Digital Craftsman ( Artist / Developer / Designer )</p>
         </Box>
