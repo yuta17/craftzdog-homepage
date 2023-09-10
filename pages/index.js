630f16581554929de9d4e318b551eee1efa048e7
yuta17 <usgitan@gmail.com>
2023-09-10 14:14:59 +0900

    Fix

--- pages/index.js
+++ pages/index.js
@@ -104,7 +104,7 @@ const Home = () => (
           <Link href="https://www.youtube.com/watch?v=7-InFoUHtO8" target="_blank">
             ピアノ
           </Link>
-          、ポーカー、格闘技の観戦 (RIZIN)、散歩、蕎麦
+          、ポーカー、格闘技の観戦 (RIZIN)、自然
         </Paragraph>
       </Section>
 
